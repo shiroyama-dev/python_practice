d97a4f46599263cae20ebf0cef1971db44aff0ce
shiroyama-dev <325788852+shiroyama-dev@users.noreply.github.com>
2026-09-11 17:54:35 +0900

    Add Python practice examples

---
+++ practice/Eric.py
@@ -0,0 +1,22 @@
+friend_name = "Eric"
+message = f"こんにちは{friend_name}, 今日はPythonを学びますか?"
+print(message)
+
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+print('アルベルト・アインシュタインは”挫折を経験したことがない者は、何も新しいことに挑戦したことがないということだ。"といった')
+
+famous_person = "アルベルト・アインシュタイン"
+message = f"{famous_person}は'挫折を経験したことがない者は、何も新しいことに挑戦したことがないということだ'と言った。"
+print(message)
+
+name = "\tada lovelace\n"
+print(name.rstrip())
+print(name.lstrip())
+print(name.strip())
+
+name = "python_notes.txt"
+print(name.removesuffix(".txt"))
